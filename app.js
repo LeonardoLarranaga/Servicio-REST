d34262b21a221ef669a7afb139d4d6ad027f9d99
Leonardo <luis.larranaga@uabc.edu.mx>
2024-04-01 11:28:50 -0700

    Implementación de CORS.

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 // importamos Express, se inicializa y elegimos el puerto a utilizar.
 import express from "express"
+import https from "https"
+import fs from "fs"
+import cors from "cors"
+
 const app = express()
+app.use(cors())
 const port = 4000
 
 // se importan las rutas para los activos, responsables y ubicaciones.
@@ -8,9 +13,17 @@ import activos from "./rutas/activos.js"
 import responsables from "./rutas/responsables.js"
 import ubicaciones from "./rutas/ubicaciones.js"
 
-// se inicia el app.
-app.listen(port, () => {
-    console.log(`Servidor escuchando por el puerto: http://localhost:${port}`)
+// credenciales OpenSSL
+const credenciales = {
+    key: fs.readFileSync("server.key"),
+    cert: fs.readFileSync("server.cer")
+}
+
+// se inicia el servidor.
+const server = https.createServer(credenciales, app)
+
+server.listen(port, () => {
+    console.log(`Servidor escuchando por el puerto: https://localhost:${port}`)
 }).on("error", error => {
     console.log("Error al iniciar el servidor:", error)
 })
